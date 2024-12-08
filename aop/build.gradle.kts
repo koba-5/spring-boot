ca93865b3f253aeb504bc48f8cb502bf50aff210
koba-5 <s-kobata.ju@outlook.com>
2024-12-08 12:11:07 +0900

    [aop] first commit

---
+++ aop/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+	id("com.diffplug.spotless") version "7.0.0.BETA4"
+}
+
+group = "aop"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Kotlin × Spring boot
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+spotless {
+	kotlin {
+		ktfmt()
+		targetExclude("build/generated/source/apollo/server/autogen/graphql/**/*.kt")
+	}
+}
+
+tasks.compileKotlin{
+	dependsOn(tasks.spotlessApply)
+}
